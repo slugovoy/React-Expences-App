019594771f5d8ff0af98ecdfff002558b0a746e8
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-01-02 18:29:01 -0700

    added add transaction

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,12 +4,7 @@ import AppReducer from "./AppReducer";
 // Initial state
 
 const initialState = {
-    transactions: [
-          { id: 1, text: 'Flower', amount: -20 },
-          { id: 2, text: 'Salary', amount: 300 },
-          { id: 3, text: 'Book', amount: -10 },
-          { id: 4, text: 'Camera', amount: 150 }
-        ]
+    transactions: []
 }
 
 
@@ -30,12 +25,21 @@ export const GlobalProvider =({ children }) => {
             payload: id
         })
     }
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        })
+    }
+
+    
 
 
     return(
         <GlobalContext.Provider value={{
             transactions: state.transactions,
-            deleteTransaction
+            deleteTransaction,
+            addTransaction
         }}>
             {children}
         </GlobalContext.Provider>
