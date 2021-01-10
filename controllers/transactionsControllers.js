659767f8bfa8e96de52972c06ab3b342cf2a4712
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-01-10 16:07:04 -0700

    added delete route

--- controllers/transactionsControllers.js
+++ controllers/transactionsControllers.js
@@ -34,12 +34,47 @@ exports.addTransaction = async (req, res, next) => {
             data: transaction
         })
     } catch (error) {
-        console.error(error)
+        if(error.name === "ValidationError"){
+          const messages = Object.values(error.errors).map(val => val.message)
+
+          return res.status(400).json({
+              success: false,
+              error: messages
+          })
+        } else {
+            return res.status(500).json({
+                success: false,
+                error: "Server Error"
+            })
+        }
     }
 }
 // @desc Delete transaction
 // @route Delete /api/v1/transactions/:id
 // @access Public
 exports.deleteTransaction = async (req, res, next) => {
-    res.send('DELETE transactions')
+   try {
+       const transaction = await Transaction.findById(req.params.id)
+
+
+       if(!transaction){
+           return res.status(404).json({
+               success: false,
+               error: "No transaction found"
+           })
+       }
+
+       await transaction.remove()
+
+       return res.status(200).json({
+           success: true,
+           data: {}
+       })
+
+   } catch (error) {
+    return res.status(500).json({
+        success: false,
+        error: "Server Error"
+    })
+   }
 }
