5338e9d29962272531ae62246648da517e81b97e
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-01-10 15:42:44 -0700

    added config, controllers, models

---
+++ controllers/transactionsControllers.js
@@ -0,0 +1,45 @@
+const Transaction =require('../models/Transaction')
+
+
+// @desc Get all transactions
+// @route GET /api/v1/transactions
+// @access Public
+exports.getTransactions = async (req, res, next) => {
+  try {
+      const transactions = await Transaction.find()
+
+      return res.status(200).json({
+          success: true,
+          count: transactions.length,
+          data: transactions
+      })
+  } catch (error) {
+      return res.status(500).json({
+          success: false,
+          error: "Server Error"
+      })
+  }
+}
+// @desc Add  transaction
+// @route POST /api/v1/transactions
+// @access Public
+exports.addTransaction = async (req, res, next) => {
+    try {
+        const { text, amount } = req.body
+
+        const transaction = await Transaction.create(req.body)
+
+        return res.status(201).json({
+            success: true,
+            data: transaction
+        })
+    } catch (error) {
+        console.error(error)
+    }
+}
+// @desc Delete transaction
+// @route Delete /api/v1/transactions/:id
+// @access Public
+exports.deleteTransaction = async (req, res, next) => {
+    res.send('DELETE transactions')
+}
