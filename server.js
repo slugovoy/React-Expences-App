85d98c5a6452a414d0aa3ef14e6de001892ddd2a
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2021-01-11 22:10:53 -0700

    added path

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if(process.env.NODE_ENV !== 'development'){
 app.use('/api/v1/transactions', transactions)
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.resolve(__dirname, './client/build')))
+    app.use(express.static(path_join(__dirname, 'client/build')))
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
